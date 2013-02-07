b6aa4bd15063eae9593d4fc171b6e7628f42e640
Ramkumar Ramachandra <artagnon@gmail.com>
2013-02-07 15:52:53 +0530

    {9,10}/foss.in: Add build.el, Makefile
    Write a clever Emacs Lisp script to use .org/ from the toplevel
    directory to org-export-as-pdf.  Wrap it in a simple Makefile, taking
    care to generate bibliographies where applicable.

---
+++ 10/foss.in/build.el
@@ -0,0 +1,28 @@
+#!/usr/bin/emacs --script
+
+(add-to-list 'load-path "../../.org/lisp")
+(load-library "org")
+
+(unless (boundp 'org-export-latex-classes)
+  (setq org-export-latex-classes nil))
+(add-to-list 'org-export-latex-classes
+	     '("beamer"
+	       "\\documentclass[8pt]{beamer}
+\\beamertemplateballitem
+\\usepackage{hyperref}
+\\usepackage{color}
+\\usepackage{listings}
+\\usepackage{natbib}
+\\usepackage{upquote}
+\\usepackage{amsfonts}
+\\lstset{frame=single, basicstyle=\\ttfamily\\small, upquote=false, columns=fixed, breaklines=true, keywordstyle=\\color{blue}\\bfseries, commentstyle=\\color{red}, numbers=left, xleftmargin=2em}"
+	       ("\\section{%s}" . "\\section*{%s}")
+	       ("\\begin{frame}[fragile]\\frametitle{%s}"
+		"\\end{frame}"
+		"\\begin{frame}[fragile]\\frametitle{%s}"
+		"\\end{frame}")))
+
+(with-current-buffer (find-file-noselect "git-object-store.org")
+  (unwind-protect
+       (progn (org-export-as-pdf nil))
+    (kill-this-buffer)))
---
+++ 9/foss.in/build.el
@@ -0,0 +1,33 @@
+#!/usr/bin/emacs --script
+
+(add-to-list 'load-path "../../.org/lisp")
+(load-library "org")
+
+(unless (boundp 'org-export-latex-classes)
+  (setq org-export-latex-classes nil))
+(add-to-list 'org-export-latex-classes
+	     '("beamer"
+	       "\\documentclass[8pt]{beamer}
+\\beamertemplateballitem
+\\usepackage{hyperref}
+\\usepackage{color}
+\\usepackage{listings}
+\\usepackage{natbib}
+\\usepackage{upquote}
+\\usepackage{amsfonts}
+\\lstset{frame=single, basicstyle=\\ttfamily\\small, upquote=false, columns=fixed, breaklines=true, keywordstyle=\\color{blue}\\bfseries, commentstyle=\\color{red}, numbers=left, xleftmargin=2em}"
+	       ("\\section{%s}" . "\\section*{%s}")
+	       ("\\begin{frame}[fragile]\\frametitle{%s}"
+		"\\end{frame}"
+		"\\begin{frame}[fragile]\\frametitle{%s}"
+		"\\end{frame}")))
+
+(with-current-buffer (find-file-noselect "haskell-internals.org")
+  (unwind-protect
+       (progn (org-export-as-pdf nil))
+    (kill-this-buffer)))
+
+(with-current-buffer (find-file-noselect "unladen-swallow.org")
+  (unwind-protect
+       (progn (org-export-as-pdf nil))
+    (kill-this-buffer)))
