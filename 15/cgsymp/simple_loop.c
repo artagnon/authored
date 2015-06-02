2cf0c93c077d35873d8f3aa8252d6e10ad54bb34
Ramkumar Ramachandra <artagnon@gmail.com>
2015-06-02 16:46:32 -0400

    cg symposium: cancel the talk

--- 15/cgsymp/simple_loop.c
+++
@@ -1,7 +0,0 @@
-void simple_loop(double Y1[100])
-{
-  int i;
-  for (i = 0; i < 100; i++) {
-    Y1[i] = 1.0 + ((1.0 + (double)i) - 1.0);
-  }
-}
--- 15/cgsymp/simple_loop.m
+++
@@ -1,7 +0,0 @@
-function Y1 = simple_loop 
-    X1 = 1:1000; 
-    Y1 = zeros(1, 100); 
-    for i = 1:100 
-        Y1(i) = X1(i); 
-    end 
-end
